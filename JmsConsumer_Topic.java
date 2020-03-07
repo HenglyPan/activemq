727a58a90be5853d8e03fd6db5179afb37ce11cc
pxd <1521388510@qq.com>
2020-03-07 22:34:02 +0800

    设置持久化topic 在feature上

--- JmsConsumer_Topic.java
+++ JmsConsumer_Topic.java
@@ -6,34 +6,42 @@ import java.io.IOException;
 public class JmsConsumer_Topic {
     private static final String ACTIVEMQ_URL = "tcp://192.168.0.128:61616";
     public static final String TOPIC_NAME = "topic01";
+
+    private static final String ACTIVEMQ_TOPIC_NAME= "Topic-Persist";
     public static void main(String[] args) throws JMSException, IOException {
 
-        System.out.println("我是1号消费者");
+        System.out.println("我是1号消费者张三");
+        //创建连接工厂
         ActiveMQConnectionFactory activeMQConnectionFactory=new ActiveMQConnectionFactory(ACTIVEMQ_URL);
-
+        //创建连接
         Connection connection = activeMQConnectionFactory.createConnection();
-        connection.start();
+        connection.setClientID("ppp");
+      //  connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         //创建目的地 topic 或queue
-        Topic topic = session.createTopic(TOPIC_NAME);
+        Topic topic = session.createTopic(ACTIVEMQ_TOPIC_NAME);
+
+        //通过session创建持久化订阅
+        TopicSubscriber topicSubscriber = session.createDurableSubscriber(topic, "我是张三");
+        connection.start();
 
         //创建消息的消费者
-        MessageConsumer consumer = session.createConsumer(topic);
+       // MessageConsumer consumer = session.createConsumer(topic);
         //指定消费哪一个队列里的消息
-        consumer.setMessageListener(message -> {
+        topicSubscriber.setMessageListener(message -> {
             if (message instanceof TextMessage){
                 try {
                     TextMessage textMessage = (TextMessage) message;
                     String text = textMessage.getText();
-                    System.out.println("消费者接受到TOPIC的消息"+text);
+                    System.out.println("接收到持久化订阅者消息"+text);
                  } catch (JMSException e) {
                  e.printStackTrace();
                  }
                }
             });
                  System.in.read();
-                 consumer.close();
+                 topicSubscriber.close();
                  connection.close();
                  session.close();
 
--- JmsProducer_Topic.java
+++ JmsProducer_Topic.java
@@ -6,18 +6,22 @@ public class JmsProducer_Topic {
     private static final String ACTIVEMQ_URL = "tcp://192.168.0.128:61616";
     //主题的名字
     public static final String TOPIC_NAME = "topic01";
+
+    private static final String ACTIVEMQ_TOPIC_NAME = "Topic-Persist";
     public static void main(String[] args) throws JMSException {
         //创建connection工厂
         ActiveMQConnectionFactory activeMQConnectionFactory=new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         //创建connection
         Connection connection = activeMQConnectionFactory.createConnection();
-        connection.start();
-        //创建session
+        //connection.start();
+        //创建session 两个参数transacted=事务,acknowledgeMode=确认模式(签收)
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         //创建目的地有queue 和topic
-        Topic topic = session.createTopic(TOPIC_NAME);
+        Topic topic = session.createTopic(ACTIVEMQ_TOPIC_NAME);
         //创建消西的生产者
         MessageProducer messageProducer = session.createProducer(topic);
+       //设置持久化
+        messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
         //使用消息生产者 生长三条消息 发送到mq的队列里
         for (int i = 0; i <3 ; i++) {
             //通过session创建消息
